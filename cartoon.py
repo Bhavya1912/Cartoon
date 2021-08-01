165bf9cb1c968ab63a40840da10386cec37672b1
BHAVYA TYAGI <72131872+Bhavya1912@users.noreply.github.com>
2021-08-01 17:03:28 +0530

    Update cartoon.py

--- cartoon.py
+++ cartoon.py
@@ -99,4 +99,4 @@ upload=Button(top,text="Cartoonify an Image",command=upload,padx=10,pady=5)
 upload.configure(background='#364156', foreground='cyan',font=('calibri',10,'bold'))
 upload.pack(side=TOP,pady=50)
 
-top.mainloop()
+top.mainloop()                       
